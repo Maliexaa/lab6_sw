e38078d13f283ca89baf7da0826b668a2822e276
Mallak Hesham <M.Hesham2246@nu.edu.eg>
2024-11-14 01:07:59 +0200

    Update square_calculator.py

--- square_calculator.py
+++ square_calculator.py
@@ -1,6 +1,8 @@
 class SquareCalculator:
     def area(self, side):
+        # Correctly calculates area (side * side)
         return side * side
 
     def perimeter(self, side):
+        # Correctly calculates perimeter (4 * side)
         return side * 4
